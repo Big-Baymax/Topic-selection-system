98ff2d62e65bd09a25e2eb3e39dc3c508b13468e
suiyuan <1982204777@qq.com>
2018-04-15 11:14:18 +0800

    数据库seed修改

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,25 +13,11 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         \Illuminate\Support\Facades\DB::table('administrators')->insert([
-            'name' => '张三',
-            'mobile' => '13211111111',
+            'name' => '超级管理员',
+            'mobile' => '13328942695',
             'login_name' => 'admin',
             'password' => '9e220fd633e62b119615e0bdcaf01d8b',
             'salt' => 'oZbzvJ+chEdaU(SE'
-        ]);
-        \Illuminate\Support\Facades\DB::table('teachers')->insert([
-            'teacherNo' => 't01',
-            'name' => '李四',
-            'sex' => 1,
-            'password' => 'e159c0d677a749cd3ed2e8fdf0425878',
-            'salt' => 'iU&WEa+lVlQg@*zV',
-        ]);
-        \Illuminate\Support\Facades\DB::table('students')->insert([
-            'stuNo' => 's01',
-            'name' => '王五',
-            'sex' => 1,
-            'password' => '6feb295428d1f31c04b9dda8761d9efe',
-            'salt' => 'rZU%&AJ(@n$+u*mx',
         ]);
     }
 }
